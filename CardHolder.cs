570c3aff330a372089e955b6fdea8ad0fa43bcfc
MilenKostov2024 <milen.kostov1717@gmail.com>
2024-11-19 13:23:56 +0200

    Add files via upload

---
+++ CardHolder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class CardHolder
+    {
+        public string CardNum { get; set; }        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Balance { get; set; }
+
+        public CardHolder(string cardNum, string firstName, string lastName, decimal balance)
+        {
+            CardNum = cardNum;            
+            FirstName = firstName;
+            LastName = lastName;
+            Balance = balance;
+        }
+
+        public CardHolder()
+        {
+            CardNum = "";            
+            FirstName = "";
+            LastName = "";
+            Balance = 0;
+
+        }
+
+        public void PrintOptions()
+        {
+            Console.WriteLine("Please choose one of the following options:");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        public void Deposit(CardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like do deposit?");
+            decimal deposit = decimal.Parse(Console.ReadLine());
+            currentUser.Balance += deposit;
+            Console.WriteLine("Thank you for your deposit. You new balance is: " + currentUser.Balance);
+        }
+
+        public void Withdraw(CardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like do withdraw?");
+            decimal withdraw = decimal.Parse(Console.ReadLine());
+            if (currentUser.Balance < withdraw)
+            {
+                Console.WriteLine("Insufficient Balance :(");
+            }
+            else
+            {
+                currentUser.Balance -= withdraw;
+                Console.WriteLine("Your balance is: " + Balance);
+                Console.WriteLine("You are good to go! Thank you :)");
+            }
+        }
+
+        public void ShowBalance(CardHolder currentUser)
+        {
+            Console.WriteLine("Current Balance: " + currentUser.Balance);
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ATM;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardHolder cardHolder = new CardHolder();
+            Console.WriteLine("ATM");
+            Console.WriteLine("Enter number on debit card: ");
+            string cardNum = Console.ReadLine();
+            Console.WriteLine("Enter your name on card: ");
+            string cardHolderName = Console.ReadLine();            
+
+            Console.WriteLine("Welcome " + cardHolderName + " :)");
+            int option = 0;
+            do
+            {
+                cardHolder.PrintOptions();
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, please enter a number.");
+                    continue;
+                }
+                if (option == 1)
+                {
+                    cardHolder.Deposit(cardHolder);
+                }
+                else if (option == 2)
+                {
+                    cardHolder.Withdraw(cardHolder);
+                }
+                else if (option == 3)
+                {
+                    cardHolder.ShowBalance(cardHolder);
+                }
+                else if (option == 4)
+                {
+                    Console.WriteLine("Thank you! Have a nice day :)");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option, please choose again.");
+                }
+            } while (option != 4);            
+        }
+    }
+}
+                   
